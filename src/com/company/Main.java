21f30028f994b7b45618300ddd1bb447906e26d2
Gabriel Morberg <gabrielmorberg13@gmail.com>
2023-06-22 15:04:20 +0200

    removing some weird things

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.awt.*;
 import javax.swing.*;
 
-public class Main extends JFrame {
+public class Main {
 
 
     public static void main(String[] args) {
@@ -39,26 +39,5 @@ public class Main extends JFrame {
 
 
 
-    }
-
-    private static void initializeUi() {
-
-        JFrame grid = new JFrame();
-        grid.setSize(800,800);
-        grid.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        grid.setVisible(true);
-
-        JPanel menu = new JPanel();
-        menu.setSize(500,500);
-        JButton star = new JButton("Star");
-        JButton sephamore = new JButton("Sephamore");
-        JButton glider = new JButton("Glider");
-        JButton reset = new JButton("Reset");
-
-        menu.add(star);
-        menu.add(sephamore);
-        menu.add(glider);
-        menu.add(reset);
-        grid.add(menu);
     }
 }
